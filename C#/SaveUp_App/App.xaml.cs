e34d1d9bbd2e0c88e37e6aa3b8f3841c48efb937
PonixXD <62787694+PonixXD@users.noreply.github.com>
2024-12-20 14:33:50 +0100

    project data

---
+++ C#/SaveUp_App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SaveUp_App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
